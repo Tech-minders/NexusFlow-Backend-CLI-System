45d5fcb7de348217a372e85ab7456a2608dc7ef7
Patrick Mutua <pawatechsys@gmail.com>
2026-02-26 08:27:44 +0300

    add services in the service file

--- subscription/services.py
+++ subscription/services.py
@@ -1,7 +1,23 @@
 
 SERVICES = {
-    "1": {"name": "ChatGPT", "url": "https://chatgpt.com/"},
+    "1": {
+        "name": "ChatGPT",
+        "url": "https://chatgpt.com/",
+        "packages": {
+            "1": {"name": "Hourly", "price": 30, "hours": 1},
+            "2": {"name": "Daily", "price": 100, "hours": 24},
+            "3": {"name": "Weekly", "price": 500, "hours": 168},
+            "4": {"name": "Monthly", "price": 2000, "hours": 720},
+        }
+    },
+
+    "2": {
+        "name": "Grammarly",
+        "url": "https://grammarly.com/",
+        "packages": {
+            "1": {"name": "Daily", "price": 30, "hours": 24},
+            "2": {"name": "Weekly", "price": 150, "hours": 168},
+            "3": {"name": "Monthly", "price": 500, "hours": 720},
+        }
+    }
 }
-#Current not used in our CLI project
-#It's reserved for future multi-service support
-#We're currently working with a single service: chatGPT
